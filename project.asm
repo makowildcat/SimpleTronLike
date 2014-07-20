e3ab74ce14b9b23bee36922c579b54ec22ce32e5
makowildcat <makowildcat@gmail.com>
2014-07-20 21:56:09 +0200

    autoRun, Cursor, Rainbow mode and Grid ok with new reading keyboard

--- project.asm
+++ project.asm
@@ -7,9 +7,10 @@ pile    ENDS
 
 data 	SEGMENT public		
 	mode 	DW	01h
+	autoRun	DB	1
 	color 	DB	04h
-	dirX	DB	1
-	dirY	DB	1
+	dirX	DW	1
+	dirY	DW	1
 	delay 	DW 	1024
 data 	ENDS
 
@@ -21,32 +22,28 @@ MOV AL, 13h
 MOV AH, 0
 INT 10h
 
-MOV AH, 0Ch
-
-; init variable
-MOV mode, 01h
-MOV color, 64
-JMP mainLoop
+; MOV AH, 0Ch
+JMP initVar 
 
 ;**** Sub ReadChar *****
-readChar:
-	;mov AH, 07h
-	;int 21H
-	MOV AH, 01h
-	INT 16h
-	RET
+; readChar:
+	; mov AH, 07h
+	; int 21H
+	; MOV AH, 01h
+	; INT 16h
+	; RET
 
 ; *** Sub ReadPxl ***
-readPxl:
-	MOV AH, 0Dh
-	RET
+; readPxl:
+	; MOV AH, 0Dh
+	; RET
 	
 ; *** Sub EchoChar ***
-Echochar:
-	MOV AH, 02
-	MOV DL,AL
-	INT 21H
-	RET
+; Echochar:
+	; MOV AH, 02
+	; MOV DL,AL
+	; INT 21H
+	; RET
 	
 ; *** Sub PutPxl ***
 PutPxl:
@@ -56,35 +53,70 @@ PutPxl:
 	RET
 
 ; *** Sub RmvPxl ***
-RmvPxl:
-	mov AH, 0Ch
-	mov BL , AL
-	mov AL, 0		; black color
-	int 10h
-	mov AL, BL
-	RET
+; RmvPxl:
+	; mov AH, 0Ch
+	; mov BL , AL
+	; mov AL, 0		; black color
+	; int 10h
+	; mov AL, BL
+	; RET
 
-screenReset:
+; screenReset:
+	; MOV AH, 0Ch
+	; MOV AL, 0
+	; MOV CX, 0
+	; MOV DX, 0
+; loopScreenReset:
+	; int 10h
+	; INC CX
+	; CMP CX, 320
+	; jne loopScreenReset
+	; MOV CX, 0
+	; INC DX
+	; CMP DX, 200
+	; jne loopScreenReset	
+	; MOV CX, 32
+	; MOV DX, 32
+	; MOV AL, 15
+
+updateData proc
+	CMP mode, 1
+	JE rainbow
+	CMP mode, 2
+	JNE noClear
 	MOV AH, 0Ch
 	MOV AL, 0
-	MOV CX, 0
-	MOV DX, 0
-loopScreenReset:
-	int 10h
-	INC CX
-	CMP CX, 320
-	jne loopScreenReset
-	MOV CX, 0
-	INC DX
-	CMP DX, 200
-	jne loopScreenReset	
-	MOV CX, 32
-	MOV DX, 32
-	MOV AL, 15
-	
-MOV CX, 0
-MOV DX, 0
-	
+	INT 10h
+	JMP noClear
+	rainbow:
+		CMP color, 54
+		JNG upColor
+		MOV color, 31
+		upColor:
+			INC color
+	noClear:
+		MOV AH, 0Ch
+		MOV AL, color
+		ADD CX, dirX
+		ADD DX, dirY
+		INT 10h
+		CMP autoRun, 1
+		JE yes_autoRun
+		MOV dirX, 0
+		MOV dirY, 0
+		yes_autoRun:
+			RET
+updateData endp
+
+initVar:
+MOV CX, 50
+MOV DX, 50
+MOV dirX, 1
+MOV dirY, 0
+MOV autoRun, 0
+MOV mode, 1
+MOV color, 64
+
 mainLoop:
 	CALL handleKeyBoard
 	CALL updateData
@@ -96,140 +128,120 @@ mainLoop:
 		
 	JMP mainLoop
 
-handleKeyBoard proc
-	MOV AH, 01h		; just to check if a key is pressed
-	INT 16h
-	JZ no_
-	
-	MOV AH, 00h 	; get the key
-	INT 16h
-	yes_:
-		CMP AL, 4Bh		; narrow left
-		JE NLeft
-		CMP AL, 4Dh		; narrow right
-		JE NRight
-		CMP AL, 48h		; narrow up
-		JE NUp
-		CMP AL, 50h		; narrow down
-		JE NDown
-		CMP AL, "q"
-		JE intermediateJump
-	no_:
-	
-	RET
-handleKeyBoard endp
-
-updateData proc
-	MOV AH, 0Ch
-	MOV AL, color
-	INC CX
-	INC DX
-	INT 10h
-	RET
-updateData endp
-
-; HandleMode:
-	; CMP mode, 01h
-	; JE removePixel
-	; CMP mode, 02h
-	; JE changeColor
-	
-; HandleChar:
-	; CMP AL, 4Bh		; narrow left
-	; JE NLeft
-	; CMP AL, 4Dh		; narrow right
-	; JE NRight
-	; CMP AL, 48h		; narrow up
-	; JE NUp
-	; CMP AL, 50h		; narrow down
-	; JE NDown
-	; CMP AL, "c"
-	; JE modeCursor
-	; CMP AL, "r"
-	; JE modeRainbow
-	; CMP AL, "q"
-	; JE intermediateJump
-	; CMP AL, "s"
-	; JE screenReset
-	; CMP AL, "g"
-	; JE drawGrid
-	; JMP putPixel
-	
-; modeCursor:
-	; MOV color, 15
-	; MOV mode, 01h
-	; JMP putPixel
-	
-; modeRainbow:
-	; MOV color, 32
-	; MOV mode, 02h
-	; JMP putPixel
-	
 NLeft:
-	DEC CX
+	CMP dirX, 0		; test to avoid way back
+	JNE putPixel
+	MOV dirX, -1
+	MOV dirY, 0
 	JMP putPixel
 NRight:
-	INC CX
+	CMP dirX, 0
+	JNE putPixel
+	MOV dirX, 1
+	MOV dirY, 0
 	JMP putPixel
 NUp:
-	DEC DX
+	CMP dirY, 0
+	JNE	putPixel
+	MOV dirX, 0
+	MOV dirY, -1
 	JMP putPixel
 NDown:
-	INC DX
+	CMP dirY, 0
+	JNE putPixel
+	MOV dirX, 0
+	MOV dirY, 1
 	JMP putPixel
-		
+
 putPixel:
 	CALL PutPxl
 	JMP mainLoop
+	
+handleKeyBoard proc
+	MOV AH, 01h		; just to check if a key is pressed
+	INT 16h
+	JZ notPressed
+	
+	MOV AH, 00h 	; get the key
+	INT 16h
+	pressed:
+		CMP AH, 4Bh		; narrow left
+		JE NLeft
+		CMP AH, 4Dh		; narrow right
+		JE NRight
+		CMP AH, 48h		; narrow up
+		JE NUp
+		CMP AH, 50h		; narrow down
+		JE NDown
+		CMP AL, "a"
+		JE toggleAutoRun
+		CMP AL, "r"
+		JE rainbowMode
+		CMP AL, "c"
+		JE cursorMode
+		CMP AL, "q"
+		JE endProgram
+		CMP AL, "g"
+		JE initGrid
+	notPressed:
+	
+	RET
+handleKeyBoard endp
+	
+toggleAutoRun:
+	CMP autoRun, 0
+	JE switch
+	MOV autoRun, 0
+	JMP putPixel
+	switch:
+		MOV autoRun, 1
+	JMP putPixel
+	
+rainbowMode:
+	MOV mode, 1
+	JMP putPixel
+	
+cursorMode:
+	MOV mode, 2
+	JMP putPixel
 
-; removePixel:
-	; CALL RmvPxl
-	; JMP HandleChar
+; intermediateJump:
 	
-; changeColor:
-	; INC color
-	; CMP color, 54
-	; JG resetColor
-	; JMP HandleChar
+	; JMP endProgram
 	
-; resetColor:
-	; MOV color, 32
-	; JMP HandleChar
-
-intermediateJump:
-	mov AX, 3		; return to console mode (to avoid typing "cls" command)
-	int 10h
-	JMP endProgram
-	
-drawGrid:
+initGrid:
+	mov AH, 0Ch
 	MOV AL, 148
 	mov CX, 10
 	mov DX, 10
 	int 10h
-lineH:
-	inc CX
-	int 10h
-	cmp CX, 310
-	jne lineH
-	ADD DX, 10
-	MOV CX, 9
-	cmp DX, 200
-	jne lineH
-MOV DX, 9
-MOV CX, 10
-lineV:
-	INC DX
-	int 10h
-	CMP DX, 190
-	jne lineV
-	ADD CX, 10
+	lineH:
+		inc CX
+		int 10h
+		cmp CX, 310
+		jne lineH
+		ADD DX, 10
+		MOV CX, 9
+		cmp DX, 200
+		jne lineH
+		
 	MOV DX, 9
-	CMP CX, 320
-	jne lineV
-JMP mainLoop
+	MOV CX, 10
+	lineV:
+		INC DX
+		int 10h
+		CMP DX, 190
+		jne lineV
+		ADD CX, 10
+		MOV DX, 9
+		CMP CX, 320
+		jne lineV
+	JMP putPixel
 	
 ; *** EXIT ***
 endProgram:
+	mov AX, 3		; return to console mode (to avoid typing "cls" command)
+	int 10h
 	mov AH, 4Ch		; end of DOS program
 	mov AL, 00h
 	int 21h
